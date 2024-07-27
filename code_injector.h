f49fe6e8c6a01fcb74ff7ba2b4e3cabbba8a8541
Ofek <ofekerez@gmail.com>
2024-07-27 18:32:38 +0300

    Fixed signature

--- code_injector.h
+++ code_injector.h
@@ -11,7 +11,7 @@
 
 int inject(pid_t processId, char* shellCode);
 int attach_target_process(pid_t processId);
-int inject_shellcode(pid_t targetProcess, struct Registers targetProcessRegisters);
+int inject_shellcode(pid_t targetProcess, struct user_regs_struct targetProcessRegisters);
 int deatch_target_process(pid_t processId);
 void log_syscall_failure(int syscallReturnCode, char* error_message);
 struct user_regs_struct get_target_process_registers(pid_t processId); 
